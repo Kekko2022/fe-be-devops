f1835c0ff687ea3c8f2a43631e3da61d17c707e1
Francesco Di Iorio <francesco2000d@gmail.com>
2026-04-16 16:47:57 +0200

    LA VOLTA BUONA

--- frontend/app.py
+++ frontend/app.py
@@ -24,4 +24,4 @@ def health():
     return jsonify({"status": "ok", "service": "frontend"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8000)
